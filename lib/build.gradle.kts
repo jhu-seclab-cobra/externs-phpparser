31900f3d236b0a3f5235f4033006f188d03c09a3
SamHsu <wafts-lampoon.4o@icloud.com>
2025-04-16 01:39:11 -0400

    test: Migrate PHP parser library to new package structure
    The PHP parser library and its supporting classes were moved to a new package namespace (`edu.jhu.cobra.externs.phpparser`) for enhanced organization and clarity. Tests for key components were added to ensure functionality remains intact after the refactor.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,30 +1,37 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.kover)
     `java-library`
+    `maven-publish`
 }
 
+group = "edu.jhu.cobra"
+version = "0.1.0"
+
+val sourceJavaVersion = JavaVersion.toVersion(libs.versions.javaSource.get())
+val targetJavaVersion = JavaVersion.toVersion(libs.versions.javaTarget.get())
+
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-//    // This dependency is exported to consumers, that is to say found on their compile classpath.
-//    api(libs.commons.math3)
-//
-//    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-//    implementation(libs.guava)
     testImplementation(kotlin("test"))
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
+kotlin {
+    jvmToolchain { languageVersion.set(JavaLanguageVersion.of(sourceJavaVersion.majorVersion)) }
+    compilerOptions { jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.fromTarget(targetJavaVersion.toString()) }
+}
+
 java {
-    val srcJavaVersion = libs.versions.javaSource.get()
-    val tarJavaVersion = libs.versions.javaTarget.get()
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(srcJavaVersion))
-    }
-    sourceCompatibility = JavaVersion.toVersion(srcJavaVersion)
-    targetCompatibility = JavaVersion.toVersion(tarJavaVersion)
+    sourceCompatibility = sourceJavaVersion
+    targetCompatibility = targetJavaVersion
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications { create<MavenPublication>("maven") { from(components["java"]) } }
 }
 
