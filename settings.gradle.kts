0217fc459ded1152aeee81e9a486f7b9dc4eaf2d
samhsu-dev <35974430+samhsu-dev@users.noreply.github.com>
2026-03-24 12:39:20 -0400

    fix: Update module inclusion to avoid build name collisions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,7 @@ plugins {
 }
 
 rootProject.name = "externs-phpparser"
-include("lib")
+
+// Unique module identifier to avoid composite build name collisions (gradle/gradle#847)
+include("jhu-seclab-cobra-externs-phpparser")
+project(":jhu-seclab-cobra-externs-phpparser").projectDir = file("lib")
